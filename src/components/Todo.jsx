dd63ea9511755d593bd2982f32c6ba2267173931
Shaikh Arish Iqbal <shaikharishiqbal@gmail.com>
2025-02-04 22:34:44 +0530

    Completed Project

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,25 +1,81 @@
+import { useEffect, useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon_alter.png";
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState(
+    localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : []
+  );
+
+  const inputRef = useRef();
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+
+  const deleteTodo = (id) => {
+    setTodoList((prvTodos) => {
+      return prvTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
+  const toggle = (id) => {
+    setTodoList((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isComplete: !todo.isComplete,
+          };
+        }
+        return todo;
+      });
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoList));
+  }, [todoList]);
+
   return (
     <div className="bg-black place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl shadow-xl">
       {/************* Title **********/}
 
       <div className="flex items-center mt-7 gap-2">
         <img className="w-8" src={todo_icon} alt="" />
-        <h1 className="text-white text-3xl font-semibold">To-Do List</h1>
+        <h1 className="text-white text-3xl font-semibold">
+          {" "}
+          T a s k - T r e k
+        </h1>
       </div>
 
       {/************* input box **********/}
 
       <div className="flex items-center my-7 bg-gray-200 p-2 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
         />
-        <button className="border-none rounded-full bg-yellow-400 w-32 h-14 text-white text-lg font-medium cursor-pointer shadow-xl">
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-yellow-400 w-32 h-14 text-white text-lg font-medium cursor-pointer shadow-xl"
+        >
           ADD +
         </button>
       </div>
@@ -27,8 +83,18 @@ const Todo = () => {
       {/************* todo list **********/}
 
       <div>
-        <TodoItems text="Learn Coding" />
-        <TodoItems text="Learn Coding with Arish" />
+        {todoList.map((items, index) => {
+          return (
+            <TodoItems
+              key={index}
+              text={items.text}
+              id={items.id}
+              isComplete={items.isComplete}
+              deleteTodo={deleteTodo}
+              toggle={toggle}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -2,15 +2,33 @@ import tick from "../assets/tick.png";
 import not_tick from "../assets/not_tick.png";
 import delete_icon from "../assets/delete.png";
 
-const TodoItems = ({ text }) => {
+const TodoItems = ({ text, id, isComplete, deleteTodo, toggle }) => {
   return (
     <div className="flex items-center my-3 gap-2">
-      <div className="flex flex-1 items-center cursor-pointer">
-        <img className="w-7" src={tick} alt="" />
-        <p className="text-slate-400 ml-4 text-[17px]">{text}</p>
+      <div
+        onClick={() => {
+          toggle(id);
+        }}
+        className="flex flex-1 items-center cursor-pointer"
+      >
+        <img className="w-7" src={isComplete ? tick : not_tick} alt="" />
+        <p
+          className={`text-slate-300 ml-4 text-[17px] ${
+            isComplete ? "line-through" : ""
+          }`}
+        >
+          {text}
+        </p>
       </div>
 
-      <img className="w-3.5" src={delete_icon} alt="" />
+      <img
+        onClick={() => {
+          deleteTodo(id);
+        }}
+        className="w-3.5 cursor-pointer"
+        src={delete_icon}
+        alt=""
+      />
     </div>
   );
 };
